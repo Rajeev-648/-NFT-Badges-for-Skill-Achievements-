05d65a1a7f85c695f87c16cd27a9a962763a7c2c
Rajeev-648 <rajivparashar06@gmail.com>
2025-04-27 16:28:09 +0530

    Update Nft.sol

--- Nft.sol
+++ Nft.sol
@@ -22,4 +22,33 @@ contract SkillBadgeNFT is ERC721URIStorage, Ownable {
     function totalBadgesIssued() public view returns (uint256) {
         return _tokenIds;
     }
+
+    // Function to burn a badge (revoke)
+    function revokeBadge(uint256 tokenId) public onlyOwner {
+        _burn(tokenId);
+    }
+
+    // Check if a user owns a specific badge
+    function ownsBadge(address user, uint256 tokenId) public view returns (bool) {
+        return ownerOf(tokenId) == user;
+    }
+
+    // Update token URI (metadata)
+    function updateTokenURI(uint256 tokenId, string memory newTokenURI) public onlyOwner {
+        _setTokenURI(tokenId, newTokenURI);
+    }
+
+    // List all badges owned by a user (inefficient on-chain, best used off-chain)
+    function badgesOfOwner(address owner) public view returns (uint256[] memory) {
+        uint256 balance = balanceOf(owner);
+        uint256[] memory result = new uint256[](balance);
+        uint256 count = 0;
+        for (uint256 i = 1; i <= _tokenIds; i++) {
+            if (_exists(i) && ownerOf(i) == owner) {
+                result[count] = i;
+                count++;
+            }
+        }
+        return result;
+    }
 }
